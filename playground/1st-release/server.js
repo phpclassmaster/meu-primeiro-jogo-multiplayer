6cadf41a9554822347a0316de20508e2a407a289
phpclassmasters <phpclassmasters@gmail.com>
2024-05-21 14:33:47 -0300

    alteracao porta 3030

--- playground/1st-release/server.js
+++ playground/1st-release/server.js
@@ -38,6 +38,6 @@ sockets.on('connection', (socket) => {
     })
 })
 
-server.listen(3000, () => {
-    console.log(`> Server listening on port: 3000`)
+server.listen(3030, () => {
+    console.log(`> Server listening on port: 3030`)
 })
